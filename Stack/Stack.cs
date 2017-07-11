5d00fe1c39a7817e02fc4cd66e5d083e178e0d92
szdodo <szeghi.@gmail.com>
2017-07-11 10:56:20 +0200

    almost done

---
+++ Stack/Stack.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack
+{
+    public class Stack<T>
+    {
+        private int Size;
+        private int StackSize;
+        private T[] stackyStack;
+
+        public Stack(int StackSize)
+        {
+            Size = 0;
+            this.StackSize = StackSize;
+            stackyStack = new T[StackSize];
+        }
+
+        public void push(T newData)
+        {
+            if (Size + 1 != StackSize)
+            {
+                stackyStack[Size] = newData;
+                Size++;
+            }
+            else
+            {
+                throw new ArgumentException("The stack is already full");
+            }
+        }
+
+        public T pop()
+        {
+            if (Size == 0)
+            {
+                throw new ArgumentException("The stack is empty");
+            }
+            T lastItem = stackyStack[Size-1];
+            Size--;
+            Array.Resize(ref stackyStack, Size);
+            Array.Resize(ref stackyStack, StackSize);
+            return lastItem;
+        }
+
+        public T peek()
+        {
+            if (Size == 0)
+            {
+                throw new ArgumentException("The stack is empty");
+            }
+            return stackyStack[Size-1];
+        }
+
+        public int getSize()
+        {
+            return StackSize;
+        }
+
+        public int getUsedSize()
+        {
+            return Size;
+        }
+
+        public int getFreeSpaces()
+        {
+            return StackSize - Size;
+        }
+
+
+
+
+    }
+}
---
+++ StackTests/StackTests.cs
@@ -0,0 +1,168 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Stack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack.Tests
+{
+    [TestClass()]
+    public class StackTests
+    {
+        [TestMethod()]
+        public void StackTestNotNull()
+        {
+            Stack<int> testStack = new Stack<int>(5);
+            Assert.IsNotNull(testStack);
+        }
+
+        [TestMethod()]
+        public void pushTestOneElement()
+        {
+            Stack<int> testStack = new Stack<int>(5);
+            testStack.push(1);
+            Assert.AreEqual(1,testStack.getUsedSize());
+        }
+
+        [TestMethod()]
+        public void pushTestSeveralElement()
+        {
+            Stack<int> testStack = new Stack<int>(5);
+            testStack.push(1);
+            testStack.push(1);
+            testStack.push(1);
+            testStack.push(1);
+            Assert.AreEqual(4, testStack.getUsedSize());
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException), "The stack is already full")]
+        public void pushTestToFullStack()
+        {
+            Stack<int> testStack = new Stack<int>(5);
+            testStack.push(1);
+            testStack.push(1);
+            testStack.push(1);
+            testStack.push(1);
+            testStack.push(1);
+            testStack.push(6);
+        }
+
+        [TestMethod()]
+        public void popTestReturnsTheRightElementOneItem()
+        {
+            Stack<int> testStack = new Stack<int>(5);
+            testStack.push(1);
+            Assert.AreEqual(1,testStack.pop());
+        }
+
+        [TestMethod()]
+        public void popTestReturnsTheRightElementSeveralItem()
+        {
+            Stack<int> testStack = new Stack<int>(5);
+            testStack.push(1);
+            testStack.push(3);
+            testStack.push(5);
+            Assert.AreEqual(5, testStack.pop());
+        }
+
+        [TestMethod()]
+        public void popTestUsedSizeChanged()
+        {
+            Stack<int> testStack = new Stack<int>(5);
+            testStack.push(1);
+            testStack.push(21);
+            testStack.push(24);
+            testStack.pop();
+            Assert.AreEqual(2, testStack.getUsedSize());
+        }
+
+        [TestMethod()]
+        public void popTestSizeUnchanged()
+        {
+            Stack<int> testStack = new Stack<int>(5);
+            testStack.push(1);
+            testStack.push(21);
+            testStack.push(24);
+            testStack.pop();
+            Assert.AreEqual(5, testStack.getSize());
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException), "The stack is empty")]
+        public void popTestEmtyStackException()
+        {
+            Stack<int> testStack = new Stack<int>(5);
+            testStack.pop();
+        }
+
+        [TestMethod()]
+        public void peekTestReturnsRightElementOneItem()
+        {
+            Stack<int> testStack = new Stack<int>(5);
+            testStack.push(1);
+            Assert.AreEqual(1, testStack.peek());
+        }
+
+        [TestMethod()]
+        public void peekTestReturnsRightElementSeveralItem()
+        {
+            Stack<int> testStack = new Stack<int>(5);
+            testStack.push(4);
+            testStack.push(21);
+            testStack.push(4);
+            testStack.push(2);
+            Assert.AreEqual(2, testStack.peek());
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException), "The stack is empty")]
+        public void peekTestEmtyStackException()
+        {
+            Stack<int> testStack = new Stack<int>(5);
+            testStack.peek();
+        }
+
+        [TestMethod()]
+        public void getSizeTest()
+        {
+            Stack<int> testStack = new Stack<int>(5);
+            Assert.AreEqual(5, testStack.getSize());
+        }
+
+        [TestMethod()]
+        public void getUsedSizeTestEmptyStack()
+        {
+            Stack<int> testStack = new Stack<int>(5);
+            Assert.AreEqual(0, testStack.getUsedSize());
+        }
+
+        [TestMethod()]
+        public void getUsedSizeTestAlmostFullStack()
+        {
+            Stack<int> testStack = new Stack<int>(5);
+            testStack.push(4);
+            testStack.push(21);
+            testStack.push(4);
+            testStack.push(2);
+            Assert.AreEqual(4, testStack.getUsedSize());
+        }
+
+        [TestMethod()]
+        public void getFreeSpacesTestEmptyStack()
+        {
+            Stack<int> testStack = new Stack<int>(5);
+            Assert.AreEqual(5, testStack.getFreeSpaces());
+        }
+
+        /*to do full and filled
+         * [TestMethod()]
+        public void getFreeSpacesTestFullStack()
+        {
+            Stack<int> testStack = new Stack<int>(5);
+            Assert.AreEqual(5, testStack.getFreeSpaces());
+        }*/
+    }
+}
